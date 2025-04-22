21211160770ea1a84041c58b4720c28e05fd8225
Pavel Demidovich <dubstr1@gmail.com>
2025-04-22 17:49:00 +0300

    tests setup

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,10 +1,29 @@
 import { test } from '@playwright/test';
 
-test.describe('the second test', () => {
-    test('should navigate to the homepage', async ({ page }) => {
-        await page.goto('http://localhost:4200/');
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+});
 
-        await page.getByText(/forms/i).click();
-        await page.getByText(/form layouts/i).click();
-    });
+test.describe('Forms Navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText(/forms/i).click();
+  });
+  
+  test('should navigate to Form Layouts', async ({ page }) => {
+    await page.getByText(/form layouts/i).click();
+  });
+  
+  test('should navigate to Datepicker', async ({ page }) => {
+    await page.getByText(/datepicker/i).click();
+  });
+});
+
+test.describe('Charts Navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText('Charts', { exact: true }).click();
+  });
+  
+  test('should navigate to Echarts', async ({ page }) => {
+    await page.getByText(/echarts/i).click();
+  });
 });
