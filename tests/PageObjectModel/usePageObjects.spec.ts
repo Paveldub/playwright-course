c43093b392c8ed157c542878db142924d99e731f
Pavel Demidovich <dubstr1@gmail.com>
2025-05-07 19:30:57 +0300

    page manager

--- tests/PageObjectModel/usePageObjects.spec.ts
+++ tests/PageObjectModel/usePageObjects.spec.ts
@@ -1,33 +1,29 @@
 import { test } from '@playwright/test';
-import { NavigationPage } from '../page-objects/navigation-page';
-import { FormLayoutsPage } from '../page-objects/formLayouts';
-import { DatePickerPage } from '../page-objects/datepickerPage';
+import { PageManager } from '../page-objects/pageManager';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/');
 });
 
 test('naviget to form page', async ({ page }) => {
-    const navigateTo = new NavigationPage(page);
+    const pageManager = new PageManager(page);
 
-    await navigateTo.formLayoutsPage();
-    await navigateTo.datepickerPage();
-    await navigateTo.smartTablePage();
-    await navigateTo.toastrPage();
-    await navigateTo.tooltipPage();
+    await pageManager.navigateTo().formLayoutsPage();
+    await pageManager.navigateTo().datepickerPage();
+    await pageManager.navigateTo().smartTablePage();
+    await pageManager.navigateTo().toastrPage();
+    await pageManager.navigateTo().tooltipPage();
 });
 
 test("parametrized methods test", async({ page }) => {
-    const navigateTo = new NavigationPage(page);
-    const onFormLayoutPage = new FormLayoutsPage(page);
-    const onDatepickerPage = new DatePickerPage(page);
+    const pageManager = new PageManager(page);
 
-    await navigateTo.formLayoutsPage();
+    await pageManager.navigateTo().formLayoutsPage();
 
-    await onFormLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "welcome1", "option 2");
-    await onFormLayoutPage.submitInlineFormWithNameEmailAndCheckbox('Pavel', "test@gmail.com", true);
+    await pageManager.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "welcome1", "option 2");
+    await pageManager.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('Pavel', "test@gmail.com", true);
 
-    await navigateTo.datepickerPage();
-    await onDatepickerPage.selectCommonDatepickerDateFromToday(5);
-    await onDatepickerPage.selectDatepickerWithRangeFromToday(5, 15);
+    await pageManager.navigateTo().datepickerPage();
+    await pageManager.onDatePickerPage().selectCommonDatepickerDateFromToday(5);
+    await pageManager.onDatePickerPage().selectDatepickerWithRangeFromToday(5, 15);
 });
--- tests/page-objects/navigationPage.ts
+++ tests/page-objects/navigationPage.ts
@@ -1,7 +1,7 @@
 import { Page } from '@playwright/test'
 
 export class NavigationPage {
-    readonly page: Page;
+    private readonly page: Page;
 
     constructor(page: Page) {
         this.page = page;
