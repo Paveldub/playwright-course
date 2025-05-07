981bd37b9c10f31b32a471138b4036605ce55aab
Pavel Demidovich <dubstr1@gmail.com>
2025-05-07 16:34:41 +0300

    POM locators

--- tests/page-objects/navigation-page.ts
+++ tests/page-objects/navigation-page.ts
@@ -1,8 +1,21 @@
-import { Page } from '@playwright/test'
+import { Locator, Page } from '@playwright/test'
 
 export class NavigationPage {
-
     readonly page: Page;
+    readonly formLayoutsMenuItem: Locator;
+    readonly datepickerMenuItem: Locator;
+    readonly smartTableMenuItem: Locator;
+    readonly toastrMenuItem: Locator;
+    readonly tooltipMenuItem: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.formLayoutsMenuItem = page.getByText(/form layouts/i);
+        this.datepickerMenuItem = page.getByText(/datepicker/i);
+        this.smartTableMenuItem = page.getByText(/smart table/i);
+        this.toastrMenuItem = page.getByText(/toastr/i);
+        this.tooltipMenuItem = page.getByText(/tooltip/i);
+    }
 
     private async selectGroupMenuItem(groupItemTitle: string) {
         const groupMenuItem = this.page.getByTitle(groupItemTitle);
@@ -13,32 +26,28 @@ export class NavigationPage {
         }
     }
     
-    constructor(page: Page) {
-        this.page = page;
-    }
-
     async formLayoutsPage() {
-        await this.selectGroupMenuItem("Forms")
-        await this.page.getByText("Form Layouts").click();
+        await this.selectGroupMenuItem("Forms");
+        await this.formLayoutsMenuItem.click();
     }
 
     async datepickerPage() {
-        await this.selectGroupMenuItem("Forms")
-        await this.page.getByText(/datepicker/i).click();
+        await this.selectGroupMenuItem("Forms");
+        await this.datepickerMenuItem.click();
     }
 
     async smartTablePage() {
-        await this.selectGroupMenuItem("Tables & Data")
-        await this.page.getByText(/smart table/i).click();
+        await this.selectGroupMenuItem("Tables & Data");
+        await this.smartTableMenuItem.click();
     }
 
     async toastrPage() {
-        await this.selectGroupMenuItem("Modal & Overlays")
-        await this.page.getByText(/toastr/i).click();
+        await this.selectGroupMenuItem("Modal & Overlays");
+        await this.toastrMenuItem.click();
     }
 
     async tooltipPage() {
-        await this.selectGroupMenuItem("Modal & Overlays")
-        await this.page.getByText(/tooltip/i).click();
+        await this.selectGroupMenuItem("Modal & Overlays");
+        await this.tooltipMenuItem.click();
     }
 }
