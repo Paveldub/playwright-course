1a4f271aca3c230e18944bf1ad389708e06cfd34
Pavel Demidovich <dubstr1@gmail.com>
2025-05-07 16:07:15 +0300

    navigation page object model design pattern

--- tests/page-objects/navigation-page.ts
+++ tests/page-objects/navigation-page.ts
@@ -3,13 +3,42 @@ import { Page } from '@playwright/test'
 export class NavigationPage {
 
     readonly page: Page;
+
+    private async selectGroupMenuItem(groupItemTitle: string) {
+        const groupMenuItem = this.page.getByTitle(groupItemTitle);
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded');
+
+        if (expandedState === "false") {
+            await groupMenuItem.click();
+        }
+    }
     
     constructor(page: Page) {
         this.page = page;
     }
 
     async formLayoutsPage() {
-        await this.page.getByText("Forms").click();
+        await this.selectGroupMenuItem("Forms")
         await this.page.getByText("Form Layouts").click();
+    }
+
+    async datepickerPage() {
+        await this.selectGroupMenuItem("Forms")
+        await this.page.getByText(/datepicker/i).click();
+    }
+
+    async smartTablePage() {
+        await this.selectGroupMenuItem("Tables & Data")
+        await this.page.getByText(/smart table/i).click();
+    }
+
+    async toastrPage() {
+        await this.selectGroupMenuItem("Modal & Overlays")
+        await this.page.getByText(/toastr/i).click();
+    }
+
+    async tooltipPage() {
+        await this.selectGroupMenuItem("Modal & Overlays")
+        await this.page.getByText(/tooltip/i).click();
     }
 }
