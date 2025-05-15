7d1351bbf6d7b7b1685c57d3484de2e49de4c1d3
Pavel Demidovich <dubstr1@gmail.com>
2025-05-15 13:41:15 +0300

    mobile and desktop tests

--- tests/testMobile.spec.ts
+++ tests/testMobile.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from '@playwright/test';
   
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
     await page.goto('/');
-    await page.locator('.sidebar-toggle').click();
+
+    if (testInfo.project.name === 'mobile') {
+        await page.locator('.sidebar-toggle').click();
+      }
   });
   
 test.describe('Form layout page', () => {
     test.beforeEach(async ({ page }) => {
       await page.getByText(/forms/i).click();
       await page.getByText(/form layouts/i).click();
-      await page.locator('.sidebar-toggle').click();
+
     })
   
-    test('input fields', async ({ page }) => {
+    test('input fields', async ({ page }, testInfo) => {
+      if (testInfo.project.name === 'mobile') {
+        await page.locator('.sidebar-toggle').click();
+      }
+
       const usingTheGridEmailInput = page.locator("nb-card", { hasText: /using the grid/i }).getByRole('textbox', { name: /email/i });
   
       await usingTheGridEmailInput.fill('test@gmail.com');
